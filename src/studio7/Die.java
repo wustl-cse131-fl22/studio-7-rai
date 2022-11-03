fc2e7601e86586a9c6e5041415279d4d0aad3c03
maevecollins <maevecollins@10.232.148.179>
2022-11-03 14:00:06 -0500

    done

--- src/studio7/Die.java
+++ src/studio7/Die.java
@@ -1,10 +1,26 @@
 package studio7;
 
 public class Die {
+	private int n;
+	
+	public Die(int n) {
+		this.n = n;
+	}
+	
+	public int roll() {
+	
+		return (int) (Math.random()*(n+1));
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Die X = new Die(15);
+		System.out.println(X.roll());
+		
 
 	}
+	
+	
 
 }
